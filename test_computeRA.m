65a797ced7208fb4398c816ad6d4c4a709a2dcdf
Mark Wilkinson <markwilkinson@Marks-MacBook-Pro.local>
2020-07-22 21:56:59 -0500

    test computeRA now gives some more realistic results

--- test_computeRA.m
+++ test_computeRA.m
@@ -2,27 +2,56 @@
 
 clear, clc
 
-ownship = [55 24 8000];
-own_vel = [3 2 0];
-target = [54.5 21 8000];
-target_v = [3 2 0];
+% ownship = [55 24 8000];
+% own_vel = [3 2 0];
+% target = [54.5 21 8000];
+% target_v = [3 2 0];
 
-p180 = pi/180;
-ftom = 0.3048;
-o_lat = ownship(1)*p180;
-o_long = ownship(2)*p180;
-o_alt = ownship(3)*ftom;
-t_lat = target(1)*p180;
-t_long = target(2)*p180;
-t_alt = target(3)*ftom;
+load('Profile1.mat')
 
-own_llh = [o_lat o_long o_alt];
-t_llh = [t_lat t_long t_alt];
-own_ecf = wgs_to_ecef(own_llh);
-tar_ecf = wgs_to_ecef(t_llh);
+profile.time = []; % not sure what this is used for yet.
 
-s_vector = ec2enu([tar_ecf own_ecf own_llh]);
-r_vector = relVelocityVector([own_vel target_v]);
-ra_thresh = RA_thresholds(ownship(3));
+% fill the signal vectors
+for n = 1:6
+    profile.o_signals(:,n) = o_profile(:,n);
+    profile.t_signals(:,n) = t_profile(:,n);
+end
 
-RA = computeRA([s_vector r_vector ra_thresh]);
+% process each signal
+% s = zeros(12,1);
+% s(1:6) = profile.o_signals(1,:);
+% s(7:12) = profile.t_signals(1,:);
+
+RAs_TAs = zeros(120,2);
+for n = 1:120
+    
+    ownship = profile.o_signals(n,1:3);
+    own_vel = profile.o_signals(n,4:6);
+    target = profile.t_signals(n,1:3);
+    target_v = profile.t_signals(n,4:6);
+    
+    p180 = pi/180;
+    ftom = 0.3048;
+    o_lat = ownship(1)*p180;
+    o_long = ownship(2)*p180;
+    o_alt = ownship(3)*ftom;
+    t_lat = target(1)*p180;
+    t_long = target(2)*p180;
+    t_alt = target(3)*ftom;
+
+    own_llh = [o_lat o_long o_alt];
+    t_llh = [t_lat t_long t_alt];
+    own_ecf = wgs_to_ecef(own_llh);
+    tar_ecf = wgs_to_ecef(t_llh);
+
+    s_vector = ec2enu([tar_ecf own_ecf own_llh]);
+    r_vector = relVelocityVector([own_vel target_v]);
+    ra_thresh = RA_thresholds(ownship(3));
+    ta_thresh = TA_thresholds(ownship(3));
+
+    RA = computeRA([s_vector r_vector ra_thresh]);
+    TA = computeTA([s_vector r_vector ta_thresh]);
+    RAs_TAs(n,1) = RA;
+    RAs_TAs(n,2) = TA;
+end
+
